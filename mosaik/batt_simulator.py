b2f5b9d3263c97040e063c92e7e2978af4661455
aguag <guagliardi@inwind.it>
2023-10-26 11:18:04 +0200

    con mosaik 3, test funzionante

---
+++ mosaik/batt_simulator.py
@@ -0,0 +1,90 @@
+"""
+Mosaik interface for the example simulator.
+
+"""
+
+#import import_ipynb
+import mosaik_api
+import batt_model
+
+META = {
+    'type': 'time-based',
+    'models': {
+        'ExampleModel': {
+            'public': True,
+            'params': ['init_val'],
+            'attrs': ['load_current', 'output_voltage'],
+        },
+    },
+}
+
+
+class ExampleSim(mosaik_api.Simulator):
+    def __init__(self):
+        super().__init__(META)
+        self.eid_prefix = 'Model_'
+        self.entities = {}  # Maps EIDs to model instances/entities
+        self.time = 0
+
+    def init(self, sid, time_resolution, eid_prefix=None):
+        if float(time_resolution) != 1.:
+            raise ValueError('ExampleSim only supports time_resolution=1., but'
+                             ' %s was set.' % time_resolution)
+        if eid_prefix is not None:
+            self.eid_prefix = eid_prefix
+        return self.meta
+
+    def create(self, num, model):
+        next_eid = len(self.entities)
+        entities = []
+
+        for i in range(next_eid, next_eid + num):
+#            model_instance = batt_model.Model(init_val)
+            model_instance = batt_model.Model()
+            eid = '%s%d' % (self.eid_prefix, i)
+            self.entities[eid] = model_instance
+            entities.append({'eid': eid, 'type': model})
+
+        return entities
+
+
+    def step(self, time, inputs, max_advance):
+        self.time = time
+       # Check for new delta and do step for each model instance:
+        for eid, model_instance in self.entities.items():
+            if eid in inputs:
+                attrs = inputs[eid]
+                for attr, values in attrs.items():
+                    new_delta = sum(values.values())
+                model_instance.delta = new_delta
+            
+            sampling_time=model_instance.sampling_time
+            model_instance.step(sampling_time, time)
+
+        return time + sampling_time  # Step size is 1 second
+
+    def get_data(self, outputs):
+        data = {}
+        for eid, attrs in outputs.items():
+            model = self.entities[eid]
+            data['time'] = self.time
+            data[eid] = {}
+            for attr in attrs:
+                if attr not in self.meta['models']['ExampleModel']['attrs']:
+                    raise ValueError('Unknown output attribute: %s' % attr)
+
+                # Get model.val or model.delta:
+                data[eid][attr] = getattr(model, attr)
+
+        return data
+
+
+def main():
+    return mosaik_api.start_simulation(ExampleSim())
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
