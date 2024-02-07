218ef43042b27c089fd5de40231a8252960aff1f
aguag <guagliardi@inwind.it>
2024-02-07 11:37:31 +0100

    Con loop mqtt ristrutturato

--- mosaik/ScenarioDT_with_mng_loop.py
+++ mosaik/ScenarioDT_with_mng_loop.py
@@ -90,9 +90,11 @@ def prepScenario(SIM_CONFIG):
 #world.run(until=END,rt_factor=1.1)
 
 def taskRun():
+    # run normale, nel processo del thread principale
     world.run(until=END,rt_factor=1.1)
 
 def taskRunInThread1(): 
+    # apertura di un thread figlio e run nel nuovo thread e check per essere fermto dopo tre iterazioni
     tRun = Thread(target=taskRun)
     tRun.start()
     sleep(1)
@@ -103,12 +105,13 @@ def taskRunInThread1():
         print(f"-----CHECK-TIMEOUT---Simulazione in RUN-{world.tqdm.total}------------------------------------------------------------------------------------------------------------>")
         tRun.join(timeout=1)  # mi aggancio al thread ed aspetto max 1 secondo, se ha finito esco, se non ha finito allora vado in loop
         if i > 3: 
-            world.until = 1  # fermo (brutalmente) la simulazione
+            world.until = 1  # fermo (brutalmente) la simulazione dopo 3 iterazioni
             
             print(f"Fermo tutto i={i}")
         i=i+1
 
 def taskRunInThread2(): 
+    # apertura di un thread figlio e run nel nuovo thread, non apsetto la fine del nuovo thread 
     tRun = Thread(target=taskRun)
     tRun.start()
     sleep(1)
@@ -136,47 +139,135 @@ def taskRunInThread2():
 ## sys.exit()
 
 class DTmqtt:
-    def __init__(self, name, mqttBroker, user, passwd, on_message):
+    def __init__(self, name, mqttBroker, user, passwd, config_data_path, configDTName ):
         self.mqttBroker = mqttBroker
         self.client = mqtt.Client(name)
-        self.client.on_message = on_message
+#        self.client.on_message = on_message
         self.client.username_pw_set(user, passwd)
-
+        self.config_data_path = "mosaik/configuration/"
+        self.configDTName = "configDT.yaml"
+        self.configDT = readConfig(self.config_data_path, self.configDTName)
+        self.tags.callbacks = self.configDT['mqtt']['DTSDA']['CALLBACK']
+        for tag in self.tags.callbacks:
+            print(f'{self.tags.callbacks[tag]}')
+            self.client.subscribe(self.tags.callbacks[tag])
+    
+        self.tags.misure = self.configDT['mqtt']['DTSDA']['MISURE']['BATT1']
+        for tag in self.tags.misure:
+            print(f'{self.tags.misure[tag]}')
+            self.client.subscribe(self.tags.misure[tag])
+    
     def connect(self):
         ret= self.client.connect(self.mqttBroker)
-        self.client.loop_start()
+#        self.client.loop_start()
         return(ret)
  
     def disconnect(self):
         return(self.client.loop_stop())
-
  
     def subscribe(self, topic):
         return(self.client.subscribe(topic))
     
 
+def on_EndProg(client, userdata, message):
+    print(f'------------------------>on_EndProg:  DISCONNET and END')
+    client.disconnect()
+
+def on_SetModoSIM(client, userdata, message):
+    print(f'------------------------>on_SetModoSIM:  SetModoSIM')
+
+def on_SetModoLEARN(client, userdata, message):
+    print(f'------------------------>on_SetModoLEARN:  SetModoLEARN')
+
+def on_LoadSIM(client, userdata, message):
+    global world, model, dtsdamng
+    print(f'------------------------>on_LoadSIM:   LOAD DELLA SIMULAZIONE')
+    world, model, dtsdamng = prepScenario(SIM_CONFIG)
 
 
-def DTmqtt_messloop():   
+def on_RunSIM(client, userdata, message):
+    global world, model, dtsdamng, tRun_on_message
+    print(f'------------------------>on_RunSIM:  RUN DELLA SIMULAZIONE')
+    #------------------------------------------
+    # Per sopprime il traceback!!!!
+    sys.tracebacklimit = 0
+    try:
+        tRun_on_message = taskRunInThread2()
+    except Exception as e:
+        if debug:
+            raise # re-raise the exception
+                  # traceback gets printed
+        else:
+            print("{}: {}".format(type(e).__name__, e))
+    sys.tracebacklimit = 1
+    # FINE soppressione traceback!!!!
+    #-----------------------------------------
+
+def on_StopSIM(client, userdata, message):
+    global world, model, dtsdamng, tRun_on_message
+    print(f'------------------------>on_StopSIM:  Stop della Simulazione')
+    world.until = 1
+    sleep(1)
+    print(f"SHUTDOWN!")       
+    try:
+        world.shutdown()
+    except Exception as e:
+        if debug:
+            raise # re-raise the exception, traceback gets printed                 
+        else:
+            print("{}: {}".format(type(e).__name__, e))
+
+def on_PlotGraf(client, userdata, message):
+    global world, model, dtsdamng, tRun_on_message
+    print(f'------------------------>on_PlotGraf:  PLOTGRAF per Debug')
+    ## grafici attivati con il debug mode
+    mosaik.util.plot_dataflow_graph(world, folder='util_figures')
+    mosaik.util.plot_execution_graph(world, folder='util_figures')
+    mosaik.util.plot_execution_time(world, folder='util_figures')
+    mosaik.util.plot_execution_time_per_simulator(world, folder='util_figures')
+
+
+def on_connect(client, userdata, flags, rc):
+   print(f"Connected With Result Code {rc}")
+
+
+def dt_mqtt_messloop():   
 #    mqttBroker = "mqtt.eclipseprojects.io"   # mqtt brocker free di prova
     mqttBroker = "0.0.0.0"     # mqtt brocker in localhost
     client = mqtt.Client("DTSDA")
+    client.on_connect = on_connect
     client.on_message = on_message  # callback dei messaggi
     client.username_pw_set("userDT", "userDT")
 
     ret = client.connect(mqttBroker)
 
-    client.subscribe("DTSDA/temperatura")
-    client.subscribe("DTSDA/tensione")
-    client.subscribe("DTSDA/corrente")
-    client.subscribe("DTSDA/SIM")
-    client.subscribe("DTSDA/LEARN")
-    client.subscribe("DTSDA/RUNSIM")
-    client.subscribe("DTSDA/STOPSIM")
-    client.subscribe("DTSDA/LOADSIM")
-    client.subscribe("DTSDA/END")
-    client.subscribe("DTSDA/PLOTGRAF")
-    #client.loop_start()
+    config_data_path = "mosaik/configuration/"
+    configDTName = "configDT.yaml"
+    configDT = readConfig(config_data_path, configDTName)
+
+#configDT['mqtt']['DTSDA']['CALLBACK']['SetModoSIM'] ---> 'DTSDA/SET_SIM'
+#configDT['mqtt']['DTSDA']['MISURE']['BATT1']['temperatura'] ---> 'DTSDA/BATT1/T'
+
+    callbacks = configDT['mqtt']['DTSDA']['CALLBACK']
+    for tag in callbacks:
+        print(f'{callbacks[tag]}')
+        client.subscribe(callbacks[tag])
+    
+    misure = configDT['mqtt']['DTSDA']['MISURE']['BATT1']
+    for tag in misure:
+        print(f'{misure[tag]}')
+        client.subscribe(misure[tag])
+    
+
+    client.message_callback_add(callbacks['EndProg'], on_EndProg)
+    client.message_callback_add(callbacks['SetModoSIM'], on_SetModoSIM)
+    client.message_callback_add(callbacks['SetModoLEARN'], on_SetModoLEARN)
+    client.message_callback_add(callbacks['LoadSIM'], on_LoadSIM)
+    client.message_callback_add(callbacks['RunSIM'], on_RunSIM)
+    client.message_callback_add(callbacks['StopSIM'], on_StopSIM)
+    client.message_callback_add(callbacks['PlotGraf'], on_PlotGraf)
+
+
     print("------------------------> DTmqtt_messloop: loop forever")
     client.loop_forever()
     print("------------------------> DTmqtt_messloop: FINE metodo")
@@ -218,7 +309,7 @@ def CheckForTmax(world, model, dtsdamng, tRun, TMAX):
 
 
 def SIMtest():
-    #tRun = taskRunInThread2()
+    # Viene eseguito un transitorio in batch e visulaizzati i plot di prestazione
     taskRun()
     ## grafici attivati con il debug mode
     mosaik.util.plot_dataflow_graph(world, folder='util_figures')
@@ -231,66 +322,7 @@ def on_message(client, userdata, message):
     # callback per messaggi MQTT
     global world, model, dtsdamng, tRun_on_message
     print(f'------------------------>on_message: Received con topic:{message.topic} message: {str(message.payload.decode("utf-8"))}')
-    
-    if message.topic == 'DTSDA/STOPSIM':
-        print(f'------------------------>on_message:  FERMO LA SIMULAZIONE')
-        world.until = 1
-        sleep(1)
-        print(f"SHUTDOWN!")       
-        try:
-            world.shutdown()
-        except Exception as e:
-            if debug:
-                raise # re-raise the exception
-                      # traceback gets printed
-            else:
-                print("{}: {}".format(type(e).__name__, e))
-
-    elif  message.topic == 'DTSDA/RUNSIM':     
-        print(f'------------------------>on_message:  RUN DELLA SIMULAZIONE')
-        #------------------------------------------
-        # Per sopprime il traceback!!!!
-        sys.tracebacklimit = 0
-        try:
-            tRun_on_message = taskRunInThread2()
-        except Exception as e:
-            if debug:
-                raise # re-raise the exception
-                      # traceback gets printed
-            else:
-                print("{}: {}".format(type(e).__name__, e))
-
-
-        sys.tracebacklimit = 1
-        # FINE soppressione traceback!!!!
-        #-----------------------------------------
-
-    elif  message.topic == 'DTSDA/LOADSIM':     
-        print(f'------------------------>on_message:  LOAD DELLA SIMULAZIONE')
-        world, model, dtsdamng = prepScenario(SIM_CONFIG)
-
-    elif  message.topic == 'DTSDA/SIM':     
-        print(f'------------------------>on_message:  MODO SIMULAZIONE')
-        pass
-
-    elif  message.topic == 'DTSDA/LEARN':
-        print(f'------------------------>on_message:  MODO LEARN')
-        pass
         
-    elif  message.topic == 'DTSDA/END':
-        print(f'------------------------>on_message:  DISCONNET and END')
-        client.disconnect()
-
-    elif  message.topic == 'DTSDA/PLOTGRAF':
-        print(f'------------------------>on_message:  PLOTGRAF per Debug')
-        ## grafici attivati con il debug mode
-        mosaik.util.plot_dataflow_graph(world, folder='util_figures')
-        mosaik.util.plot_execution_graph(world, folder='util_figures')
-        mosaik.util.plot_execution_time(world, folder='util_figures')
-        mosaik.util.plot_execution_time_per_simulator(world, folder='util_figures')
-        
-    else:
-        pass
 
 
 def main():
@@ -304,7 +336,8 @@ def main():
         END=10
         SIMtest()
     else:
-        DTmqtt_messloop()
+        # modalità normale in "mqtt message loop"
+        dt_mqtt_messloop()
 
 
 #-------------------------------------------------------------------------
@@ -357,5 +390,4 @@ if __name__ == '__main__':
 # world.sims['ModelSim-0']._proxy.sim.entities['Model_0'].load_current
 # world.sims['ModelSim-0'].output_to_push[('Model_0', 'DTmode_set')][0][1]  ritorna il valore di DTmode_set
 # world.sims['ModelSim-0'].current_step.time         tempo corrente della task ModelSim-0
-
 #------------------------------------------------------
