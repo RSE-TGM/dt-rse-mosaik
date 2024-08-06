ea015fc89e4d8bf8c7c5bbf16bfb00b49149f2de
Antonio Guagliardi <guagliardi@rse-web.it>
2024-08-06 12:02:29 +0200

    newmod versione funzionante. to be debugged

--- app/batt_simulator.py
+++ app/batt_simulator.py
@@ -26,21 +26,47 @@ else:
 
 from src.digital_twin.orchestrator.base_manager import GeneralPurposeManager
 
+# META_Battmodel = {
+# #    'type': 'time-based',
+#     'type': 'hybrid',
+#     'models': {
+#         'BattModel': {
+#             'public': True,
+#             'params': ['init_val'],
+#             'attrs': ['load_current', 
+#                       'output_voltage',
+#                       'DTmode_set',
+#                       'DTmode'],
+#             'trigger': ['DTmode'],                # input: trigger per gli eventi
+#             'non-persistent': ['DTmode_set'],     # output: non-persistent per gli eventi
+#         },
+#     },
+# }
+
 META_Battmodel = {
-#    'type': 'time-based',
-    'type': 'hybrid',
-    'models': {
-        'BattModel': {
-            'public': True,
-            'params': ['init_val'],
-            'attrs': ['load_current', 
-                      'output_voltage',
-                      'DTmode_set',
-                      'DTmode'],
-            'trigger': ['DTmode'],                # input: trigger per gli eventi
-            'non-persistent': ['DTmode_set'],     # output: non-persistent per gli eventi
-        },
-    },
+    'type': 'time-based',
+   'type': 'hybrid',
+   'models': {
+       'BattModel': {
+           'public': True,
+           'params': ['init_val'],
+           'attrs': [ 'load_current',
+                    'output_voltage',
+                    'soc',
+                    'soh',
+                    'Vocv',
+                    'power',
+                    'temperature',
+                    'heat',
+                    'C',
+                    'R0',
+                    'R1',
+                    'DTmode_set',
+                    'DTmode'],
+           'trigger': ['DTmode'],                # input: trigger per gli eventi
+           'non-persistent': ['DTmode_set'],     # output: non-persistent per gli eventi
+       },
+   },
 }
 
 
@@ -135,25 +161,36 @@ class ModelSim(mosaik_api.Simulator):
                         #model_instance.load_current = list(values.values())[0] #  era sum(values.values()), corrente allo step successivo
                         model_instance._input_var='current'
                         model_instance._ground_data[model_instance._input_var][0]=list(values.values())[0]
+                        model_instance.delta=list(values.values())[0]
             
             sampling_time=model_instance._stepsize   # model_instance.sampling_time è letto dal file di config experiment
             
             if model_instance.DTmode == MODSIM :
-                model_instance.run_step()   # sono in modo "simulazione"
+                model_instance.run_step()   # sono in modo "simulazione", eseguo lo STEP
                 modo=S_SIM
             else:
                 model_instance.learn(sampling_time, time)  # sono in modo "learn"
                 modo=S_LEARN
                
         
-        next_step = time + int(sampling_time) 
-        model_instance.load_current=9
-        model_instance.output_voltage=99
-        model_instance.delta=999
+         
+        model_instance.load_current=model_instance._results['operations']['current'][-1]
+        model_instance.output_voltage=model_instance._results['operations']['voltage'][-1]
+        model_instance.soc=model_instance._results['operations']['soc'][-1]
+        model_instance.soh=model_instance._results['operations']['soh'][-1]
+        model_instance.Vocv=model_instance._results['operations']['Vocv'][-1]
+        model_instance.power=model_instance._results['operations']['power'][-1]
+        model_instance.temperature=model_instance._results['operations']['temperature'][-1]
+        model_instance.heat=model_instance._results['operations']['heat'][-1]
+        model_instance.C=model_instance._results['operations']['C'][-1]        
+        model_instance.R0=model_instance._results['operations']['R0'][-1]
+        model_instance.R1=model_instance._results['operations']['R1'][-1]
+
+        next_time = time + int(sampling_time)
         logger.info('batt_model: time={time} sampling_time={sampling_time} - La batteria è in modalità {modo}, current[A]={current} voltage[V]={voltage} next_current[A]={next_step_current}', time=time, sampling_time=sampling_time, modo=modo, 
                     current=model_instance.load_current, voltage=model_instance.output_voltage, next_step_current=model_instance.delta)
-        print(f'------>  {model_instance._results}')
-        return next_step  # Step size, cioè sampling_time, è normalmmente 1 secondo ed è letto dal file di config experiment
+#        print(f'------>  {model_instance._results}')
+        return next_time  # Step size, cioè sampling_time, è normalmmente 1 secondo ed è letto dal file di config experiment
 
     def get_data(self, outputs):
         data = {}
